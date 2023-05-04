a24f0d64db3cb60cee524886efcd62748ce19734
balojr <77960542+balojr@users.noreply.github.com>
2023-05-04 19:34:39 +0300

    add imported modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,28 @@ import { AppComponent } from './app.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
 import { BackgroundComponent } from './components/background/background.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import {RouterModule, Routes} from "@angular/router";
+import { IntroComponent } from './components/intro/intro.component';
+import { NavComponent } from './components/nav/nav.component';
 
+const appRoutes: Routes = [
+  {path: '', component: LandingpageComponent},
+  {path: 'skills', component: SkillsComponent},
+];
 @NgModule({
   declarations: [
     AppComponent,
     LandingpageComponent,
     BackgroundComponent,
-    SkillsComponent
+    SkillsComponent,
+    IntroComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule // <-- include it under 'imports'
+    BrowserAnimationsModule, // <-- include it under 'imports'
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
