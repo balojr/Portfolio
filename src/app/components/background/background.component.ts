0c6aaa3c6db4d623d8412a797b1bdc3640aeada9
balojr <77960542+balojr@users.noreply.github.com>
2023-05-04 19:34:24 +0300

    add background styling

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { trigger, state, style, animate, keyframes, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.scss']
 })
-export class BackgroundComponent {
+export class BackgroundComponent implements OnInit{
 
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 }
