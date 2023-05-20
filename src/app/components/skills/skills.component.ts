f35e27b86ce527e15eb634a954d2fb768e3bb766
balojr <77960542+balojr@users.noreply.github.com>
2023-05-20 21:55:13 +0300

    add angular material

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -6,37 +6,10 @@ import Swiper from 'swiper';
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent implements  AfterViewInit{
-  activeDiv = 1;
+export class SkillsComponent{
+  // backImage = "./assets/images/undraw_code_thinking_re_gka2.svg"
 
   constructor() {
   }
-  ngAfterViewInit() {
-    const swiper = new Swiper('.swiper-container', {
-      slidesPerView: 3.5,
-      spaceBetween: 25,
-      loop: true,
-      speed: 1888,
-      simulateTouch: false,
-      autoplay: {
-        delay: 1800,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      on: {
-        realIndexChange: () => {
-          // Handle real index change event here
-        },
-        init: () => {
-          // Handle swiper init event here
-        },
-      },
-    });
-  }
+
 }
